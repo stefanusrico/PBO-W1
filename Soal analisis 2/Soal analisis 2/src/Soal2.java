8ef37a6b0bd7241c833df72e502ba623030b5305
stefanusrico <111185559+stefanusrico@users.noreply.github.com>
2023-08-24 08:46:39 +0700

    Update Soal2.java
    Menambah komentar

--- Soal analisis 2/Soal analisis 2/src/Soal2.java
+++ Soal analisis 2/Soal analisis 2/src/Soal2.java
@@ -5,7 +5,7 @@
 public class Soal2 {
     public static void main(String[] args) {
         int i = 42;
-        String s = (i<40)?"life":(i>50)?"universe":"everything";
+        String s = (i<40)?"life":(i>50)?"universe":"everything";  //menggunakan konsep Ternary Conditional Operator
         System.out.println(s);
     }
 }
