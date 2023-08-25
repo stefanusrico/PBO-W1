2d32f2862dba47604bdcf61cd841a9fdb0d1f47b
stefanusrico <111185559+stefanusrico@users.noreply.github.com>
2023-08-25 17:32:14 +0700

    Update Welcome.java

--- Listing 2.1/Welcome/Welcome.java
+++ Listing 2.1/Welcome/Welcome.java
@@ -6,7 +6,7 @@ public class Welcome {
     public static void main(String[] args) {
             String greeting = "Welcome to Core Java!";
             System.out.println(greeting);
-            for(int i = 0; i < greeting.length(); i++)  //menggunakan method length(), untuk menghitung setiap karakter (termasuk spasi) yang terdapat dalam variabel greeting 
+            for(int i = 0; i < greeting.length(); i++)  //menggunakan method length(), untuk menghitung setiap karakter (termasuk spasi) yang terdapat dalam variabel greeting. Panjang dari variabel greeting ialah 21 oleh karena itu akan ditampilan "=" sebanyak 21 kali
                 System.out.print("=");
             System.out.println();
     }
